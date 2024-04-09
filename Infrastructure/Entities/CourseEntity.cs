754d02904cc727261e39d9832342e3bc22606d5a
Willi <william.hagg19982@outlook.com>
2024-04-09 19:45:58 +0200

    Add project files.

---
+++ Infrastructure/Entities/CourseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class CourseEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public bool IsBestseller { get; set; }
+
+    public string Image { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public string Price { get; set; } = null!;
+
+    public string? DiscountPrice { get; set; }
+
+    public string Hours { get; set; } = null!;
+
+    public string LikesInProcess { get; set; } = null!;
+
+    public string LikesInNumbers { get; set; } = null!;
+}
