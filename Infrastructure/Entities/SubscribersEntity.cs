6d774eb98dee4ca316be23138ac32d024b50ff50
Willi <william.hagg19982@outlook.com>
2024-04-19 13:01:46 +0200

    fixed my webb api assignment

--- Infrastructure/Entities/SubscribersEntity.cs
+++ Infrastructure/Entities/SubscribersEntity.cs
@@ -6,4 +6,17 @@ public class SubscribersEntity
 {
     [Key]
     public string Email { get; set; } = null!;
+
+
+    public bool DailyNewsLetter { get; set; }
+
+    public bool AdvertisingUpdates { get; set; }
+
+    public bool WeekinReview { get; set; }
+
+    public bool EventUpdates { get; set; }
+
+    public bool StartupWeekly { get; set; }
+
+    public bool Podcasts { get; set; }
 }
